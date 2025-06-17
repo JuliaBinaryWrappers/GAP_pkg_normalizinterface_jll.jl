d8121c67cbc3b2f029b7b3e624fac0f72ce61de3
jlbuild <juliabuildbot@gmail.com>
2025-06-17 16:30:16 +0000

    GAP_pkg_normalizinterface_jll build 100.300.701+0

--- src/wrappers/aarch64-linux-gnu-cxx03.jl
+++ src/wrappers/aarch64-linux-gnu-cxx03.jl
@@ -2,12 +2,11 @@
 export NormalizInterface
 
 using GAP_jll
-using GAP_lib_jll
 using normaliz_jll
 JLLWrappers.@generate_wrapper_header("GAP_pkg_normalizinterface")
 JLLWrappers.@declare_file_product(NormalizInterface)
 function __init__()
-    JLLWrappers.@generate_init_header(GAP_jll, GAP_lib_jll, normaliz_jll)
+    JLLWrappers.@generate_init_header(GAP_jll, normaliz_jll)
     JLLWrappers.@init_file_product(
         NormalizInterface,
         "lib/gap/NormalizInterface.so",
